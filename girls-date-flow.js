c6e34214a3e9c0e2b921d623c601a6700164f19c
hugh36007-hub <hugh36007@gmail.com>
2026-08-29 23:19:17 +0100

    Load refreshed Girls payment nudge feedback

--- girls-date-flow.js
+++ girls-date-flow.js
@@ -91,4 +91,4 @@ installStyles();
 setTimeout(syncActiveForms,0);
 })();
 
-(()=>{'use strict';if(document.querySelector('script[data-payment-nudge-loader="girls"]'))return;const script=document.createElement('script');script.src='/girls-payment-nudge.js?v=1';script.async=false;script.dataset.paymentNudgeLoader='girls';document.head.append(script)})();
+(()=>{'use strict';if(document.querySelector('script[data-payment-nudge-loader="girls"]'))return;const script=document.createElement('script');script.src='/girls-payment-nudge.js?v=2';script.async=false;script.dataset.paymentNudgeLoader='girls';document.head.append(script)})();
